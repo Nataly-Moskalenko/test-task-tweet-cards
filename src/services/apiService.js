8293620773e0b0f1d15e082333c41aaacc3ee82e
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-06-01 16:32:09 +0300

    Updates

--- src/services/apiService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://64579cc11a4c152cf98605f9.mockapi.io/phonebook';
-
-export default async function apiService() {
-  const response = await axios.get(`${BASE_URL}/users`);
-  return response.data;
-}
