699de26da3a5afd2eb7d255e0b8bb655e94995b6
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-06-03 22:29:47 +0300

    Updates

---
+++ static/js/471.984f0a72.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_task_tweet_cards=self.webpackChunktest_task_tweet_cards||[]).push([[471],{936:function(e,t,s){s.r(t),s.d(t,{default:function(){return i}});var c={homeTitle:"Home_homeTitle__yjPG8"},n=s(515),a=s(184);function i(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("h1",{className:c.homeTitle,children:"Welcome to Tweet Cards Application!"}),(0,a.jsx)(n.Z,{})]})}}}]);
+//# sourceMappingURL=471.984f0a72.chunk.js.map
--- static/js/727.2574741a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_task_tweet_cards=self.webpackChunktest_task_tweet_cards||[]).push([[727],{936:function(e,t,s){s.r(t),s.d(t,{default:function(){return i}});var c={homeTitle:"Home_homeTitle__yjPG8"},n=s(424),a=s(184);function i(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("h1",{className:c.homeTitle,children:"Welcome to Tweet Cards Application!"}),(0,a.jsx)(n.Z,{})]})}}}]);
-//# sourceMappingURL=727.2574741a.chunk.js.map
